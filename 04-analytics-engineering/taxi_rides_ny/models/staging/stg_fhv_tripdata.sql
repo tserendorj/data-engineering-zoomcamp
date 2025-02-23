0390a86673dc1e82b4d9610be4bb59641f8d7134
tserendorj tumen <tserendorj.tumen@gmail.com>
2025-02-23 07:37:02 +0000

    g

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -4,24 +4,19 @@
     )
 }}
 
-with tripdata as 
-(
-  select *
-      from {{ source('staging','fhv_tripdata') }}
-)
 select
 
     dispatching_base_num,    
     cast(pickup_datetime as timestamp) as pickup_datetime,
     cast(dropoff_datetime as timestamp) as dropoff_datetime,
     
-    cast(PULocationID as integer }} as pickup_locationid,
-    cast(DOLocationID as integer }}  as dropoff_locationid,
+    cast(PULocationID as integer ) as pickup_locationid,
+    cast(DOLocationID as integer )  as dropoff_locationid,
 
     
     sr_flag
     
-from tripdata
+from {{ source('staging','fhv_tripdata') }}
 
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
